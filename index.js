76ba6f4a6c177208e40ba0a2c072fb10d34023a8
emily knopf <emlbragg@Gmail.com>
2022-06-08 13:25:23 -0500

    commit tree 00558710c676a8a687a69af64428d18603bb16de

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(aNumber){
   // Write your code here!
+  if (aNumber <= 400) {
+  return 'This one is on me!' 
+  } else if (aNumber <= 2000) {
+   return 'That will be twenty bucks.';
+  } else if (2000 < aNumber && aNumber <= 2500) { ;
+    return 'I will gladly take your thirty bucks.'
+  } else { ;
+   return 'No can do.'
+  }
+}
+function ternaryCheckCity(cityMessage){
+  // Write your code here!
+ return cityMessage ==='NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch (tip) {
+  case 'generous' :
+  return 'Thank you so much.'
+  case 'not as generous' :
+  return 'Thank you.'
+  default: 
+  return 'Bye.'
+  }
 }
